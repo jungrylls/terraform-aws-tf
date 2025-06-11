09bbac12592c2dd41c47f0db3851233e25672137
Jun Yeob Kim <jungrylls@gmail.com>
2025-06-10 23:08:18 -0700

    Add type and description to bucket_name variable on 2025-06-10

--- s3/variables.tf
+++ s3/variables.tf
@@ -1,10 +1,16 @@
-variable "bucket_name" {}
+variable "bucket_name" {
+  type        = string  # Added type
+  description = "The name of the S3 bucket (must be globally unique)."
+}
 
 variable "force_destroy" {
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
+  description = "Whether to allow the bucket to be destroyed even if it contains objects."
 }
+
 variable "enable_versioning" {
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
+  description = "Whether to enable versioning on the S3 bucket."
 }
